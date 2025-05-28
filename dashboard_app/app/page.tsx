a0be622801f4b831819bf4f79bb7ca59be64f93b
geoenterprise <jovannyrey@hotmail.com>
2025-05-27 21:56:40 -0500

    week 3 chapter 16

--- dashboard_app/app/page.tsx
+++ dashboard_app/app/page.tsx
@@ -4,6 +4,11 @@ import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 // import styles from '@/app/ui/home.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Page() {
   return (
